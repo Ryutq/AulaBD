437cbbc93bc91aeab72cbb9cd35153e649e57106
ryu <139727681+Ryutq@users.noreply.github.com>
2025-11-27 13:55:47 -0300

    att

--- aluno_cadastro.php
+++ aluno_cadastro.php
@@ -4,33 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
+
     <?php include "menu.php" ?>
+
     <div class="conteudo">
-     <h3>Cadastro de Alunos</h3>
 
-    <form method="POST" action="#">
-       <p>
-        Digite o nome do aluno<br>
-        <input type="text" name="nome" required>
-       </p>
+        <h3>Cadastro de Alunos</h3>
 
-        <p>
-            Digite a cidade do aluno <br>
-            <input type="text" name="cidade">
-        </p>
+        <form action="aluno_cadastro1.php" method="post">
+            <p>
+                Digite o nome do aluno <br>
+                <input type="text" name="nome">
+            </p>
 
-        <p>
-            Digite o estado <br>
-            <input type="text" name="estado">
-        </p>
+            <p>
+                Digite a cidade do aluno <br>
+                <input type="text" name="cidade">
+            </p>
 
-        <p>
-            <input type="submit" name="enviar" value="Cadastrar">
-        </p>
-    </form>
+            <p>
+                Digite o estado <br>
+                <input type="text" name="estado">
+            </p>
+
+            <p>
+                <input type="submit" name="enviar" value="Cadastrar">
+            </p>
+        </form>
+
     </div>
+
+   
+
 </body>
 </html>
+
+               
